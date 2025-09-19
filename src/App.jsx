eaa45831c82f3738e0891d6d0cb2e8bedba99030
AmitPatelup65 <patelboy330@gmail.com>
2025-09-19 23:06:31 +0530

    Calc

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <input type="text" />
+      <button value="(">(</button>
+      <button value=")">)</button>
+      <button value="%">%</button>
+      <button value="AC">AC</button>
+      <button value="1">1</button>
+      <button value="2">2</button>
+      <button value="3">3</button>
+    </div>
+  )
+}
+
+export default App
